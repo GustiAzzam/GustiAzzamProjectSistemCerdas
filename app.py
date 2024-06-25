65f108637884f544a968635072db1ec791d6be50
GustiAzzam <muhammad1azzam@gmail.com>
2024-06-26 03:16:53 +0700

    app.py

--- app.py
+++ app.py
@@ -1,31 +1,23 @@
 import streamlit as st
-from google.cloud import speech_v1p1beta1 as speech
+import speech_recognition as sr
 
-# Fungsi untuk mengenali teks dari audio menggunakan Google Cloud Speech-to-Text
-def recognize_audio(audio_file):
-    client = speech.SpeechClient()
-
-    # Konfigurasi untuk pemrosesan audio
-    config = {
-        "encoding": speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        "sample_rate_hertz": 44100,
-        "language_code": "en-US",
-    }
-
-    # Baca file audio sebagai binary
-    content = audio_file.read()
-
-    # Request ke Google Cloud Speech-to-Text
-    audio = {"content": content}
-    response = client.recognize(config=config, audio=audio)
-
-    # Ambil hasil teks dari response
-    results = [result.alternatives[0].transcript for result in response.results]
-    return results
+# Fungsi untuk mendeteksi teks dari file audio
+def detect_audio(file):
+    recognizer = sr.Recognizer()
+    
+    try:
+        with sr.AudioFile(file) as source:
+            audio_data = recognizer.record(source)
+            text = recognizer.recognize_google(audio_data)
+            return text
+    except sr.UnknownValueError:
+        return "Suara tidak dikenali"
+    except sr.RequestError:
+        return "Tidak dapat mengakses layanan pengenalan suara"
 
 # Main function untuk aplikasi Streamlit
 def main():
-    st.title("Aplikasi Pengenalan Suara dengan Google Cloud")
+    st.title("Aplikasi Pengenalan Suara")
 
     # Upload file audio dari pengguna
     uploaded_file = st.file_uploader("Unggah file audio", type=['wav'])
@@ -33,10 +25,10 @@ def main():
     if uploaded_file is not None:
         st.audio(uploaded_file, format='audio/wav')
 
-        # Deteksi audio menggunakan Google Cloud Speech-to-Text
+        # Deteksi audio
         if st.button('Deteksi Suara'):
-            results = recognize_audio(uploaded_file)
-            st.write('Hasil Deteksi:', results)
+            result = detect_audio(uploaded_file)
+            st.write('Hasil Deteksi:', result)
 
 # Panggil main function untuk menjalankan aplikasi
 if __name__ == '__main__':
